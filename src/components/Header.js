af0814266ee1652036ed4812585bb9ca0844f08d
Travis Laudahl <travis.laudahl@gmail.com>
2019-11-22 16:55:49 -0600

    Update Header.js
    Updated the about me section

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,13 +19,13 @@ export default function Header(props) {
             <Fade top><h1>Travis Laudahl</h1></Fade>
             <div className='socials'>
                 <Fade left big><div className='email'><a href='#contact'><i id='email' title='travis.laudahl@gmail.com' className="fas fa-envelope fa-2x"></i></a><p>travis.laudahl@gmail.com</p></div></Fade>
-                <Fade right big><i id='github' title='Minnesota' className="fab fa-github fa-2x" onClick={handleClick}></i></Fade>
+                <Fade right big><i id='github' title='tlaudahl' className="fab fa-github fa-2x" onClick={handleClick}></i></Fade>
                 <Fade right big><i id='linkedin' title='travis-laudahl' className="fab fa-linkedin-in fa-2x" onClick={handleClick}></i></Fade>
                 <Fade right big><i id='twitter' title='LaudahlTravis' className="fab fa-twitter fa-2x" onClick={handleClick}></i></Fade>
             </div>
             <Fade bottom><h2>Full Stack Web Developer</h2></Fade>
             <p id='about'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet lectus sit amet risus scelerisque accumsan et ac risus. Aenean vulputate sagittis arcu. Maecenas eget fermentum est. Sed ornare facilisis dapibus. Fusce vel eros volutpat, tempor velit at, pretium urna. Praesent rhoncus eros nibh, sed sagittis ante facilisis vitae. 
+                Full Stack Web Developer with a background in the automotive field and management. Looking to turn hands-on experience in JavaScript, React, Node.js, and other web technologies into professional experience.
             </p>
         </header>
     )
