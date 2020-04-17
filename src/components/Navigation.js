e34b26aa432e7583c9202bb7d620d453c88a92f5
tlaudahl <travis.laudahl@gmail.com>
2020-04-17 15:37:03 -0500

    Added a button for project, when hovered shows a tooltip of the tech stack for that project

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,7 +8,7 @@ export default function Navigation() {
         <div className='navBar'>
             <nav className='navigation'>
                 <div className='logo'>
-                <h2>TL.D</h2>
+                <h2>TRAVIS LAUDAHL</h2>
                 </div>
                     <div className='navLinks'>
                         <a href='#home' onClick={() => ReactGA.event({
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,12 +6,12 @@ export default function Skills() {
     return (
         <section id='skills'>
             <div className='skillsDiv'>
-                <h1>Technologies</h1>
+                <h1>Skills</h1>
                 <div className='skillsContainer'>
                     <div className='skill'>React<i className="fab fa-react fa-3x"></i></div>
                     <div className='skill'>JavaScript<i className="fab fa-js-square fa-3x"></i></div>
                     <div className='skill'>Node.js<i className="fab fa-node fa-3x"></i></div>
-                    <div className='skill'>Python<i class="fab fa-python fa-3x"></i></div>
+                    <div className='skill'>Python<i className="fab fa-python fa-3x"></i></div>
                     <div className='skill'>LESS<i className="fab fa-less fa-3x"></i></div>
                     <div className='skill'>SASS<i className="fab fa-sass fa-3x"></i></div>
                     <div className='skill'>HTML5<i className="fab fa-html5 fa-3x"></i></div>
